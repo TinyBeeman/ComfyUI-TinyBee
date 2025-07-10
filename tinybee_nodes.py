e854c40a798fe7b78e2d9728cdd46ab77ff39ed2
Tony Beeman <beeman@gmail.com>
2025-07-09 20:06:53 -0700

    Initial commit

---
+++ tinybee_nodes.py
@@ -0,0 +1,184 @@
+import math
+import random
+import re
+
+class imp_listCountNode:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "string_list": ("STRING", {"forceInput": True}),
+            }
+        }
+
+    
+    @staticmethod
+    def countList(string_list):
+        return (len(string_list),)
+    
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("Count",)
+    FUNCTION = "countList"
+    CATEGORY = "🐝TinyBee"
+
+class imp_randomListEntryNode:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "seed": ("INT", {"default": -1, "min": -1, "max": 0xffffffffffffffff}),
+                "string_list": ("STRING", {"forceInput": True}),
+            }
+        }
+    
+    @staticmethod
+    def getRandomListEntry(seed, string_list):
+        if (seed[0] == -1):
+            # Use the current time as a seed
+            random.seed()
+        else:
+            random.seed(seed[0])
+
+        if (len(string_list) > 0):
+            entry = random.choice(string_list)
+            return (entry,)
+        return None
+    
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("Entry",)
+    FUNCTION = "getRandomListEntry"
+    CATEGORY = "🐝TinyBee"
+
+class imp_indexedListEntryNode:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "index": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                "string_list": ("STRING", {"forceInput": True}),
+            }
+        }
+    
+    @staticmethod
+    def getIndexedListEntry(index, string_list):
+        if (len(string_list) > 0):
+            entry = string_list[index[0] % len(string_list)]
+            return (entry,)
+        return None
+    
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("Entry",)
+    FUNCTION = "getIndexedListEntry"
+    CATEGORY = "🐝TinyBee"
+
+
+class imp_IncrementerNode:
+    def __init__(self):
+        self.counters = {}
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "start": ("INT", {"default": 0, "min": -4294967296, "max": 4294967296}),
+                "end": ("INT", {"default": 4294967296, "min": -4294967296, "max": 4294967296}),
+                "step": ("INT", {"default": 1, "min": -4294967296, "max": 4294967296}),
+            },
+            "optional": {
+                "reset_bool": ("BOOLEAN", {"default": False, "label_on": "Reset", "label_off": "Continue"}),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+            }
+        }
+
+    @classmethod
+    def IS_CHANGED(cls, **kwargs):
+        return float("NaN")
+
+    RETURN_TYPES = ("INT",)
+    RETURN_NAMES = ("counter",)
+    FUNCTION = "increment_number"
+
+    CATEGORY = "🐝TinyBee"
+
+    def increment_number(self, start, end, step, unique_id, reset_bool=0):
+        counter = int(start)
+        initialized = False
+        if reset_bool and self.counters.__contains__(unique_id):
+            self.counters.pop(unique_id)
+
+        if self.counters.__contains__(unique_id):
+            initialized = True
+            counter = self.counters[unique_id]
+
+        if (initialized):
+            counter += step
+            if (step > 0 and counter > end):
+                # end + 1 -> start, end + 2 -> start + 1, ...
+                counter = start + (counter - end - 1)
+            elif (step < 0 and counter < start):
+                # end - 1 -> start, end - 2 -> start - 1, ...
+                # If end is 1, and counter is 0, then counter should be start
+                # If end is 1, and counter is -1, then counter should be start - 1
+                counter = start - (end - counter - 1)
+                
+        self.counters[unique_id] = counter
+        return ( counter, )
+    
+class imp_indexedListEntryNode:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "index": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                "string_list": ("STRING", {"forceInput": True}),
+            }
+        }
+    
+    @staticmethod
+    def getIndexedListEntry(index, string_list):
+        if (len(string_list) > 0):
+            entry = string_list[index[0] % len(string_list)]
+            return (entry,)
+        return None
+    
+    INPUT_IS_LIST = True
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("Entry",)
+    FUNCTION = "getIndexedListEntry"
+    CATEGORY = "🐝TinyBee"
+
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "Random Entry": imp_randomListEntryNode,
+    "List Count": imp_listCountNode,
+    "Indexed Entry": imp_indexedListEntryNode,
+    "Incrementer": imp_IncrementerNode
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "imp_listCountNode": "List Count",
+    "imp_randomListEntryNode": "Random Entry",
+    "imp_indexedListEntryNode": "Indexed Entry",
+    "imp_IncrementerNode": "Incrementer"
+}
+
